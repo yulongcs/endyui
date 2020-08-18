42b7f2e199c2bc3286af980f01f3f365b1099d89
yulong <yulong@huayun.com>
2020-08-19 00:17:57 +0800

    init

---
+++ .storybook/preview.js
@@ -0,0 +1,26 @@
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
+
+import React from "react";
+import { GlobalStyle } from "../src/components/shared/global";
+import { addDecorator, addParameters } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+addParameters({
+  options: {
+    showRoots: true,
+  },
+  dependencies: {
+    withStoriesOnly: true,
+    hideEmpty: true,
+  },
+});
+addDecorator(withA11y);
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export * from "./components/shared/global";
+export * from "./components/shared/styles";
+export * from "./components/shared/animation";
+export { default as Button }  from "./components/button";
