a8185d1e1449da2697f8df59e63426319f692a97
yulongcs <yulongcs@qq.com>
2020-08-20 14:22:09 +0000

    Deploying to gh-pages from  @ 70eb644fbc150c70332e4aad3794566fc34d2d3f 🚀

---
+++ precache-manifest.30e522dbaceafab9fb34ded317efcfc4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3531ea1ee6576d2ef3ab32485de47be5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.25091c4158cf157929f1.bundle.js"
+  },
+  {
+    "url": "runtime~main.25091c4158cf157929f1.bundle.js"
+  },
+  {
+    "url": "vendors~main.25091c4158cf157929f1.bundle.js"
+  },
+  {
+    "url": "vendors~main.25091c4158cf157929f1.bundle.js.LICENSE.txt"
+  }
+]);
