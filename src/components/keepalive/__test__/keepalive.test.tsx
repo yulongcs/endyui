887d175a685580fbb0eefa44269b73dc507dfaf0
yulong <yulong@huayun.com>
2020-09-06 00:25:00 +0800

    test: 修改test测试文件

--- src/components/keepalive/__test__/keepalive.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, fireEvent, cleanup } from "@testing-library/react";
-import { Keepalive } from "../index.js";
-import { color, typography } from "../../shared/styles";
-
-describe("test Keepalive component", () => {
-    it(" ", () => {})
-})
