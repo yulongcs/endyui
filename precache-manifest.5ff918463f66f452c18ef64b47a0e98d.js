04ba4b6036cc07fcbcc7a0f3f6ac9da5d7287f2e
yulongcs <yulongcs@qq.com>
2020-09-05 16:03:42 +0000

    Deploying to gh-pages from  @ 94c71739547b33f44267092f3ce130ad0593600c 🚀

---
+++ precache-manifest.5ff918463f66f452c18ef64b47a0e98d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf0cc1f0a3f1f8877adcef78e226ca6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ddfb77836838e6e6c599.bundle.js"
+  },
+  {
+    "url": "runtime~main.ddfb77836838e6e6c599.bundle.js"
+  },
+  {
+    "url": "vendors~main.ddfb77836838e6e6c599.bundle.js"
+  },
+  {
+    "url": "vendors~main.ddfb77836838e6e6c599.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7faa8dbe6d8ddb1b49b87905e06007f5.js"
+  "precache-manifest.5ff918463f66f452c18ef64b47a0e98d.js"
 );
 
 self.addEventListener('message', (event) => {
