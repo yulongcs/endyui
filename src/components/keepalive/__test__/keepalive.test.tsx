94c71739547b33f44267092f3ce130ad0593600c
yulong <yulong@huayun.com>
2020-09-06 00:00:08 +0800

    feat: keep-alive组件

---
+++ src/components/keepalive/__test__/keepalive.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Keepalive } from "../index.js";
+import { color, typography } from "../../shared/styles";
+
+describe("test Keepalive component", () => {
+    it(" ", () => {})
+})
